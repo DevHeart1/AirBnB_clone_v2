35ec30d76c7fbe13da610e4eccd185e97bbd157d
DevHeart1 <obasipeace412@yahoo.com>
2024-02-21 14:42:30 +0000

    script that starts a Flask web application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_hbnb():
     """Method that returns Hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Method that returns HBNB"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,  debug=True)
