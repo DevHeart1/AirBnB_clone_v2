92e82f6bb833bdc55c38f4b88dae8b51478e643e
DevHeart1 <obasipeace412@yahoo.com>
2024-02-22 12:44:27 +0000

    display an int and a text

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -33,5 +33,11 @@ def python_text(text):
     return "Python {}".format(text)
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_n(n):
+    """Method that returns an integer and a text"""
+    return "{} is a number".format(n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,  debug=True)
